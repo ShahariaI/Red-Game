4212a5067b65decbaad6e0ce088de8315dd8093b
xXGoofyGooberXx <nils.nylanderbaksa@elev.ga.lbs.se>
2025-01-14 09:26:37 +0100

    wheelman funny

--- Assets/Scripts/PlayerStuff/PlayerMovement.cs
+++ Assets/Scripts/PlayerStuff/PlayerMovement.cs
@@ -44,6 +44,10 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            
+        }
+        if (rb.velocity.y <= 0)
+        {
             animator.SetBool("isJumping", false);
         }
 
