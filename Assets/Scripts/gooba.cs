8218e5bec092ed776be89743c409d20a0fc7c821
xXGoofyGooberXx <nils.nylanderbaksa@elev.ga.lbs.se>
2025-01-16 10:32:42 +0100

    ⛪

--- Assets/Scripts/gooba.cs
+++ Assets/Scripts/gooba.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class gooba : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+//no one will find your corpse
 }
