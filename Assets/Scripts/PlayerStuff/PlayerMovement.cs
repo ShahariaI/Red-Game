c0a2ca9875b749150a4b725be373c0cb8fdaef9f
Nostiny <153897581+Nostiny@users.noreply.github.com>
2025-01-17 12:49:43 +0100

    t

--- Assets/Scripts/PlayerStuff/PlayerMovement.cs
+++ Assets/Scripts/PlayerStuff/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     private float speed = 8f;
-    private float jumpingPower = 20f;
+    private float jumpingPower = 15f;
     private bool isFacingRight = true;
     private bool doubleJump;
 
