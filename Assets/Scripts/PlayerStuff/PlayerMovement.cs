64aa772f5b7c61f472881b7e655fb1e0d0916d9c
BuzzinBoi <viggo.jacobson@elev.ga.lbs.se>
2025-01-17 12:43:02 +0100

    Running takes stamina now

--- Assets/Scripts/PlayerStuff/PlayerMovement.cs
+++ Assets/Scripts/PlayerStuff/PlayerMovement.cs
@@ -21,6 +21,7 @@ public class PlayerMovement : MonoBehaviour
 
     private float runningSpeed = 15f;
     private bool isRunning;
+    private float runningCost = 5f;
 
     private float maxStamina = 100f;
     public float currentStamina = 0f;
@@ -50,7 +51,10 @@ public class PlayerMovement : MonoBehaviour
         isRunning = Input.GetKey(KeyCode.LeftControl);
         speed = isRunning ? runningSpeed : 8f;
         animator.SetBool("isRunnig", isRunning);
-        
+        if (isRunning)
+        {
+            currentStamina -= runningCost * Time.deltaTime;
+        }
 
         if (IsGrounded() && !Input.GetButton("Jump"))
         {
